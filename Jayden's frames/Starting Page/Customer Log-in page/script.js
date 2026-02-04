24a01946c9b9462de8c964884dcd2c13c3b17282
Jaydenbong <S10273809@connect.np.edu.sg>
2026-02-04 16:35:16 +0800

    Reformatted files into correct sequence

--- Jayden's frames/Starting Page/Customer Log-in page/script.js
+++
@@ -1,6 +0,0 @@
-// Frame 1 does not need much JS yet
-// This is just a placeholder for future interaction
-
-document.querySelector(".signup-btn").addEventListener("click", () => {
-    alert("Sign up page coming soon!");
-});
